69e23ff70648c384cc9021f741715bf99c806b31
Mateusz Olszewski <matcrono@gmail.com>
2021-06-19 17:33:13 +0800

    add routing

--- server.js
+++ server.js
@@ -1,64 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-var uniqid = require('uniqid');
-const db = [
-  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
-  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
-  { id: 3, author: 'Vlad Tepes', text: 'This is hard.' },
-];
-const confirmation = { message: 'OK'};
+const testimonialsRoutes = require('./routes/testimonials.routes.js')
+const seatsRoutes = require('./routes/seats.routes.js')
+const concertsRoutes = require('./routes/concerts.routes.js')
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-
-app.get('/testimonials', (req, res) => {
-  res.json(db)
-});
-
-app.get('/testimonials/:id', (req, res) => {
-  console 
-  res.json(db.find((testimony) => {
-   return +req.params.id === testimony.id 
-}))
-
-});
-
-app.get('/testimonials/random', (req, res) => {
-  const getRandomNumber = (max) => {
-    return Math.floor(Math.random() * (max + 1));
-  };
-  res.json(db[getRandomNumber(db.length)]);
-});
-
-app.post('/testimonials', (req, res) => {
-  db.push({
-    id: uniqid(),
-    author: req.body.author,
-    text: req.body.text,
-  });
-  res.json(confirmation);
-});
-
-app.put('/testimonials/:id', (req, res) => {
-  const updatedTestimony = ({
-    id: req.params.id,
-    author: req.body.author,
-    text: req.body.text,
-  });
-  const TestimonyUpdate = db.find(testimony => testimony.id == req.params.id);
-  const index = db.indexOf(TestimonyUpdate);
-  db[index] = updatedTestimony;
-  res.json(confirmation);
-});
-
-app.delete('/testimonials/:id', (req, res) => {
-  const deletedTestimony = db.find(testimony => testimony.id == req.params.id);
-  const index = db.indexOf(deletedTestimony);
-  db.splice(index, 1);
-  res.json(confirmation);
-});
+app.use('/api', testimonialsRoutes);
+app.use('/api', seatsRoutes);
+app.use('/api', concertsRoutes);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
